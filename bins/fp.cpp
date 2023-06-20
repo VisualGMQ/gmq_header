c1e43560a7117f53c60901aec50dfe0dc3136c5e
VisualGMQ <2142587070@qq.com>
2023-06-20 17:30:12 +0800

    (add): add some template implementation

--- bins/fp.cpp
+++ bins/fp.cpp
@@ -3,26 +3,42 @@
 #define CATCH_CONFIG_MAIN
 #include "3rdlibs/catch.hpp"
 
+using l = GList(1, 2, 3, 4);
+
 TEST_CASE("list operations") {
     SECTION("template implementation") {
-        using listT = List<int, 1, 2, 3, 4>;
+        using listT = GList(1, 2, 3, 4);
 
         constexpr int head = tmpl::Head<listT>;
         constexpr int snd = tmpl::Head<tmpl::Tail<listT>>;
         constexpr int last = tmpl::Last<listT>;
         using tail = tmpl::Tail<listT>;
         using init = tmpl::Init<listT>;
-        using concatedList = tmpl::Concat<listT::elem_type, 0, listT>;
+        using concatedList = tmpl::Concat<listT, 0>;
 
         static_assert(tmpl::Eq<listT, List<int, 1, 2, 3, 4>>);
         static_assert(head == 1);
         static_assert(snd == 2);
         static_assert(last == 4);
-        static_assert(tmpl::Eq<tail, List<int, 2, 3, 4>>);
-        static_assert(tmpl::Eq<init, List<int, 1, 2, 3>>);
-        static_assert(tmpl::Eq<concatedList, List<int, 0, 1, 2, 3, 4>>);
-
-
+        static_assert(tmpl::Eq<tail, GList(2, 3, 4)>);
+        static_assert(tmpl::Eq<init, GList(1, 2, 3)>);
+        static_assert(tmpl::Eq<concatedList, GList(0, 1, 2, 3, 4)>);
+        static_assert(tmpl::Nth<0, listT> == 1);
+        static_assert(tmpl::Nth<1, listT> == 2);
+        static_assert(tmpl::Nth<2, listT> == 3);
+        static_assert(tmpl::Nth<3, listT> == 4);
+        static_assert(tmpl::Null<List<int>>);
+        static_assert(tmpl::Eq<tmpl::Replicate<3, int, 1>, GList(1, 1, 1)>);
+        static_assert(tmpl::Eq<tmpl::ConcatList<GList(1, 2, 3), GList(4, 5, 6)>, GList(1, 2, 3, 4, 5, 6)>);
+        static_assert(tmpl::Eq<tmpl::_Take<3, GList(1, 2, 3, 4)>::type, GList(1, 2, 3)>);
+        static_assert(tmpl::Sum<GList(1, 2, 3)> == 6);
+        static_assert(tmpl::Maxmimum<GList(-2, 5, 9, 1, 3)> == 9);
+        static_assert(tmpl::Elem<GList(-2, 5, 9, 1, 3), -2>);
+        static_assert(tmpl::Elem<GList(-2, 5, 9, 1, 3), 5>);
+        static_assert(!tmpl::Elem<GList(-2, 5, 9, 1, 3), 0>);
+        static_assert(tmpl::Fst<Tuple<GTElem(1), GTElem(2)>> == 1);
+        static_assert(tmpl::Snd<Tuple<GTElem(1), GTElem(2)>> == 2);
+        static_assert(tmpl::Eq<tmpl::Map<GList(1, 2, 3, 4), tmpl::Inc>, GList(2, 3, 4, 5)>);
     }
 
     SECTION("compile-time function implementations") {
--- fp.hpp
+++ fp.hpp
@@ -4,17 +4,41 @@
 #include <memory>
 #include <iostream>
 #include <tuple>
+#include <array>
+#include <string_view>
 
 //! @brief basic List data structure
 //! @tparam T element type
 //! @tparam ... elements
-template <typename T, T...>
+template <typename T, T... values>
 struct List {
     using elem_type = T;
+    static constexpr std::array<T, sizeof...(values)> value = {values..., };
+};
+
+//! @brief a quick macro to generate a list type
+#define GList(...) List<decltype(__VA_ARGS__), __VA_ARGS__>
+
+//! @brief represent any compile-time value
+template <typename T, T value>
+struct TElem {
+    using type = T;
+    static constexpr T value = value;
+};
+
+#define GTElem(x) TElem<decltype(x), x>
+
+//! @brief compile-time tuple
+//! @tparam ...ElemsT must be Any<T, value>
+template <typename... ElemsT>
+struct Tuple {
+    static constexpr auto value = std::tuple<ElemsT...>{};
 };
 
 //! @brief pure template implementation
 namespace tmpl {
+
+/**********************List Operations*********************/
 
 template <typename T>
 struct _Head final { };
@@ -33,6 +57,11 @@ struct _Tail<List<T, first, Remains...>> final {
 };
 
 template <typename T>
+struct _Tail<List<T>> final {
+    using type = List<T>;
+};
+
+template <typename T>
 struct _Last final {};
 
 template <typename T, T first>
@@ -45,6 +74,7 @@ struct _Last<List<T, first, Remains...>> final {
     static constexpr T value = sizeof...(Remains) == 0 ? first : _Last<List<T, Remains...>>::value;
 };
 
+// concat a element to list
 template <typename T, typename T::elem_type>
 struct _Concat final {};
 
@@ -61,29 +91,265 @@ struct _Init <List<T, first, last>> final {
     using type = List<T, first>;
 };
 
+template <typename T>
+struct _Init <List<T>> final {
+    using type = List<T>;
+};
+
 template <typename T, T first, T... Remains>
 struct _Init<List<T, first, Remains...>> final {
     using type = typename _Concat<typename _Init<List<T, Remains...>>::type, first>::type;
 };
 
+template <size_t N, typename T>
+struct _Nth final {};
 
+
+template <typename T, T first, T... Remains>
+struct _Nth<0, List<T, first, Remains...>> final {
+    static constexpr T value = first;
+};
+
+template <size_t N, typename T, T first, T... Remains>
+struct _Nth<N, List<T, first, Remains...>> final {
+    static constexpr T value = _Nth<N - 1, List<T, Remains...>>::value;
+};
+
+template <typename T>
+struct _Length final {};
+
+template <typename T, T... Remains>
+struct _Length<List<T, Remains...>> final {
+    static constexpr size_t value = sizeof...(Remains);
+};
+
+template <typename T>
+struct _Null final { };
+
+template <typename T>
+struct _Null<List<T>> {
+    static constexpr bool value = true;
+};
+
+template <typename T, T... Remains>
+struct _Null<List<T, Remains...>> {
+    static constexpr bool value = false;
+};
+
+template <typename ListT1, typename ListT2>
+struct _ConcatList;
+
+
+template <typename T, T value, T... Remains>
+struct _ConcatList<List<T, value>, List<T, Remains...>> final {
+    using type = List<T, value, Remains...>;
+};
+
+// concat two lists
+template <typename ListT1, typename ListT2>
+struct _ConcatList final {
+    using type = typename _ConcatList<
+                    typename _Init<ListT1>::type,
+                    typename _Concat<ListT2, _Last<ListT1>::value>::type
+                >::type;
+};
+
+template <size_t N, typename T, T value>
+struct _Replicate final {
+    using type = typename _Concat<typename _Replicate<N - 1, T, value>::type, value>::type;
+};
+
+template <typename T, T value>
+struct _Replicate<1, T, value> final {
+    using type = List<T, value>;
+};
+
+template <size_t N, typename ListT>
+struct _Take;
+
+template <typename T, T... Remains>
+struct _Take<0, List<T, Remains...>> final {
+    using type = List<T>;
+};
+
+template <size_t N, typename ListT>
+struct _Take final {
+    using type = typename _Concat<
+                    typename _Take<
+                        N - 1,
+                        typename _Tail<ListT>::type
+                    >::type,
+                    _Head<ListT>::value
+                >::type;
+};
+
+template <typename ListT>
+struct _Sum;
+
+template <typename T>
+struct _Sum<List<T>> {
+    static constexpr T value = T{};
+};
+
+template <typename T, T first, T... Remains>
+struct _Sum<List<T, first, Remains...>> final {
+    static constexpr T value = first + _Sum<List<T, Remains...>>::value;
+};
+
+template <typename ListT>
+struct _Maxmimum;
+
+template <typename T, T value>
+struct _Maxmimum<List<T, value>> final {
+    static constexpr T value = value;
+};
+
+template <typename T, T first, T... Remains>
+struct _Maxmimum<List<T, first, Remains...>> final {
+    static constexpr T value = std::max(first, _Maxmimum<List<T, Remains...>>::value);
+};
+
+template <typename ListT, typename ListT::elem_type elem>
+struct _Elem {
+    static constexpr bool value = elem == _Head<ListT>::value ? true : _Elem<typename _Tail<ListT>::type, elem>::value;
+};
+
+template <typename T, T value, T elem>
+struct _Elem<List<T, value>, elem> {
+    static constexpr bool value = value == elem;
+};
+
+//! @brief get first element in list
+//! @warning can't compile when list is empty
+//! @tparam ListT list
 template <typename ListT>
 constexpr auto Head = _Head<ListT>::value;
 
+//! @brief get list without first element
+//! @warning can't compile when list is empty
+//! @tparam ListT list
 template <typename ListT>
 using Tail = typename _Tail<ListT>::type;
 
+//! @brief get last element in list
+//! @warning can't compile when list is empty
+//! @tparam ListT list
 template <typename ListT>
 constexpr auto Last = typename _Last<ListT>::value;
 
+//! @brief judge if two lists have same elements and order
+//! @tparam ListT list
 template <typename ListT1, typename ListT2>
 constexpr auto Eq = std::is_same_v<ListT1, ListT2>;
 
-template <typename T, T value, typename ListT>
+//! @brief add a value ahead of list
+//! @tparam ListT list
+//! @tparam value the value to be added
+template <typename ListT, typename ListT::elem_type value>
 using Concat = typename _Concat<ListT, value>::type;
 
+//! @brief concat two lists
+template <typename List1, typename List2>
+using ConcatList = typename _ConcatList<List1, List2>::type;
+
+//! @brief get list without latest element
 template <typename ListT>
 using Init = typename _Init<ListT>::type;
+
+//! @brief get Nth element in list
+template <size_t N, typename ListT>
+constexpr auto Nth = _Nth<N, ListT>::value;
+
+//! @brief judge if list is empty
+template <typename ListT>
+constexpr bool Null = _Null<ListT>::value;
+
+//! @brief get list length
+template <typename ListT>
+constexpr auto Length = _Length<ListT>::value;
+
+template <size_t N, typename T, T value>
+using Replicate = typename _Replicate<N, T, value>::type;
+
+template <typename ListT>
+constexpr auto Sum = _Sum<ListT>::value;
+
+template <typename ListT>
+constexpr auto Maxmimum= _Maxmimum<ListT>::value;
+
+template <typename ListT, typename ListT::elem_type value>
+constexpr auto Elem = _Elem<ListT, value>::value;
+
+
+/**********************Tuple Operations*********************/
+template <typename Tuple>
+struct _Fst;
+
+template <typename Elem1, typename Elem2>
+struct _Fst<Tuple<Elem1, Elem2>> {
+    static constexpr auto value = Elem1::value;
+};
+
+template <typename Tuple>
+struct _Snd;
+
+template <typename Elem1, typename Elem2>
+struct _Snd<Tuple<Elem1, Elem2>> {
+    static constexpr auto value = Elem2::value;
+};
+
+
+template <typename Tuple>
+constexpr auto Fst = _Fst<Tuple>::value;
+
+template <typename Tuple>
+constexpr auto Snd = _Snd<Tuple>::value;
+
+/**********************Other Common Operations*********************/
+template <typename ListT,
+          template <typename U, U value>
+          typename Func
+        >
+struct _Map;
+
+template <typename T, T value,
+            template <typename U, U value>
+            typename Func>
+struct _Map<List<T, value>, Func> {
+    using type = List<T, Func<T, value>::value>;
+};
+
+template <typename T, T value,
+            template <typename U, U value>
+            typename Func,
+          T... Remains>
+struct _Map<List<T, value, Remains...>, Func> {
+    using type = Concat<typename _Map<List<T, Remains...>, Func>::type, Func<T, value>::value>;
+};
+
+
+template <typename T, T value>
+struct Inc final {
+    static constexpr T value = value + 1;
+};
+
+template <typename T, T value>
+struct Dec final {
+    static constexpr T value = value - 1;
+};
+
+//! @brief apply function `Func` for each element
+//! @tparam Func must has template declare: `template<typename T, T value>`
+//! @tparam ListT applied list
+// template <typename ListT, typename Func>
+// using Map = typename _Map<ListT, Func>::type;
+
+
+template <typename ListT,
+          template <typename T, T value>
+          typename Func
+        >
+using Map = typename _Map<ListT, Func>::type;
 
 }
 
